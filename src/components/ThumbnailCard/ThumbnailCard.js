e9280aed86cc25339f02053350829d7c1e919fc5
Ashwin Khode <ashwin.khode1@gmail.com>
2021-04-15 05:04:13 +0530

    add: Thumbnail Card and Grid component

---
+++ src/components/ThumbnailCard/ThumbnailCard.js
@@ -0,0 +1,42 @@
+import {FaCheckCircle} from 'react-icons/fa'
+
+const ThumbnailCard = ({
+  title = 'Your Name - Official Trailer',
+  channel = 'Funanimation',
+  thumbnailURL = '../../assets/trailer1.jpg',
+  viewsCount = '6.2',
+  uploadDuration = '4',
+}) => {
+  const viewsCountRounded = String(viewsCount).charAt(0) + String(viewsCount).charAt(1)
+  return (
+    <div className='flex flex-col rounded-xl overflow-hidden bg-[#1A1A1A] shadow-thumbnailCard'>
+      <div className='w-full'>
+        <img
+          src={thumbnailURL}
+          alt={title}
+          aria-label={title}
+        />
+      </div>
+      <div
+        className='w-full p-3'>
+        <h1
+          className='font-medium opacity-90'
+        >
+          {title}
+        </h1>
+        <h2
+          className='opacity-60 inline-flex items-center gap-x-2 text-sm'>
+          {channel} <FaCheckCircle />
+        </h2>
+        <p
+          className='opacity-60 text-sm'
+        >
+          {`${viewsCountRounded}M views`} &middot; {`${uploadDuration} ago`}
+        </p>
+      </div>
+
+    </div>
+  )
+}
+
+export default ThumbnailCard
