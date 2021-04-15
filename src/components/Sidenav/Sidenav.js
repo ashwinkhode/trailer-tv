2fee47eac9a3015bd6cded397f96b806ad94e49c
Ashwin Khode <ashwin.khode1@gmail.com>
2021-04-16 01:40:01 +0530

    feat: sidenav toggle feature add

--- src/components/Sidenav/Sidenav.js
+++ src/components/Sidenav/Sidenav.js
@@ -3,13 +3,14 @@ import {Link} from 'react-router-dom'
 import {BiMenu} from 'react-icons/bi'
 import {sidenavRoutes} from '../../utils/router'
 
-export default function Sidenav() {
+export default function Sidenav({menuToggle, setMenuToggle}) {
 
   return (
-    <div className='h-full flex flex-col items-start bg-[#181818] '>
+    <div className='relative h-full flex flex-col items-start bg-[#181818] '>
       <li
         className='p-6 hover:bg-[#252525] cursor-pointer list-none'
         aria-label='Menu'
+        onClick={() => setMenuToggle(!menuToggle)}
       >
         <BiMenu className='w-6 h-6' />
       </li>
@@ -29,6 +30,39 @@ export default function Sidenav() {
             </Link>
           )
         )
+      }
+      {
+        menuToggle ? (
+          <div className='absolute w-[250%] h-full flex flex-col items-start bg-[#181818] z-10 shadow-sidenav'>
+            <li
+              className='p-6 w-full flex flex-row justify-start items-center gap-x-2 hover:bg-[#252525] cursor-pointer list-none'
+              aria-label='Menu'
+              onClick={() => setMenuToggle(!menuToggle)}
+            >
+              <BiMenu className='w-6 h-6' />
+              Menu
+            </li>
+            {
+              sidenavRoutes.map(
+                link => (
+                  <Link
+                    key={link.name}
+                    className='w-full'
+                    to={link.path}
+                  >
+                    <li
+                      className='p-6 w-full flex flex-row justify-start items-center gap-x-2 hover:bg-[#252525] cursor-pointer list-none'
+                      aria-label={link.name}
+                    >
+                      <link.icon className='w-6 h-6' />
+                      {link.name}
+                    </li>
+                  </Link>
+                )
+              )
+            }
+          </div>
+        ) : null
       }
     </div>
   )
