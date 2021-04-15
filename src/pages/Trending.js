9bfdce6e276f914c3321699808056ab579683612
Ashwin Khode <ashwin.khode1@gmail.com>
2021-04-16 01:42:12 +0530

    add: SEO for trending page

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -1,30 +1,29 @@
 import Header from '../components/Header/Header'
+import SEO from '../components/SEO/SEO'
 import ThumbnailGrid from '../components/ThumbnailGrid/ThumbnailGrid'
 
 import {latestData, popularData, recommendedData, trendingData} from '../utils/thumbnailData'
 
 const Trending = () => {
   return (
-    <div className='w-full h-full '>
-      <Header />
-      <div className='flex flex-col justify-between space-y-6 h-5/6 py-6 px-10 overflow-y-scroll scrollbars disable-scrollbars'>
-        <ThumbnailGrid
-          heading='Recommended For You'
-          thumbnailArray={recommendedData}
-        />
-        <ThumbnailGrid
-          heading='Trending Now'
-          thumbnailArray={trendingData}
-        />
-        <ThumbnailGrid
-          heading='Latest Now'
-          thumbnailArray={latestData}
-        />
-        <ThumbnailGrid
-          heading='Popular Now'
-          thumbnailArray={popularData}
-        />
-      </div>
+    <div className='flex flex-col justify-between space-y-6 h-5/6 py-6 px-10 overflow-y-scroll scrollbars disable-scrollbars'>
+      <SEO title='Trending - TrailerTV | A Platform for Trailers' />
+      <ThumbnailGrid
+        heading='Recommended For You'
+        thumbnailArray={recommendedData}
+      />
+      <ThumbnailGrid
+        heading='Trending Now'
+        thumbnailArray={trendingData}
+      />
+      <ThumbnailGrid
+        heading='Latest Now'
+        thumbnailArray={latestData}
+      />
+      <ThumbnailGrid
+        heading='Popular Now'
+        thumbnailArray={popularData}
+      />
     </div>
   )
 }
