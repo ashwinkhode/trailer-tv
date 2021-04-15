f606a25600d9de3f9c10c4d4c7c89323ed3369dc
Ashwin Khode <ashwin.khode1@gmail.com>
2021-04-16 01:40:21 +0530

    fix: header add to layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,22 @@
+import clsx from 'clsx'
+import {useState} from 'react'
+import Header from '../Header/Header'
 import Sidenav from '../Sidenav/Sidenav'
 
 export const Layout = ({children}, ...otherprops) => {
+  const [menuToggle, setMenuToggle] = useState(false)
   return (
     <div className='w-full h-full flex justify-start items-center rounded-3xl bg-black bg-opacity-40 overflow-hidden'
       {...otherprops}
     >
-      <div className='h-full'>
-        <Sidenav />
+      <div className={clsx('h-full w-[5%]')}>
+        <Sidenav menuToggle={menuToggle} setMenuToggle={setMenuToggle} />
       </div>
-      {children}
+      <div className='w-[95%] h-full flex flex-col justify-start items-start'
+        onClick={() => setMenuToggle(false)}>
+        <Header />
+        {children}
+      </div>
     </div>
   )
 }
