ef8fe83ceacd9b90ed40f4b1b711593d27cd99b6
Ashwin Khode <ashwin.khode1@gmail.com>
2021-07-11 18:43:38 +0530

    fix: routes

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -1,7 +1,7 @@
-import {BsCollectionPlay} from 'react-icons/bs'
-import {BiHomeAlt, BiMenu} from 'react-icons/bi'
-import {MdMovieFilter} from 'react-icons/md'
-import {HiFire, HiOutlineSave} from 'react-icons/hi'
+import { BsCollectionPlay } from 'react-icons/bs';
+import { BiHomeAlt, BiMenu } from 'react-icons/bi';
+import { MdMovieFilter } from 'react-icons/md';
+import { HiFire, HiOutlineSave } from 'react-icons/hi';
 import Home from '../pages/Home';
 import Trending from '../pages/Trending';
 import MyPlaylists from '../pages/MyPlaylists';
@@ -12,58 +12,58 @@ export const sidenavRoutes = [
   {
     name: 'Home',
     path: '/',
-    element: <Home />,
-    icon: BiHomeAlt
+    element: Home,
+    icon: BiHomeAlt,
   },
   {
     name: 'Trending',
     path: 'trending',
-    element: <Trending />,
-    icon: HiFire
+    element: Trending,
+    icon: HiFire,
   },
   {
     name: 'My Playlists',
     path: 'playlists',
-    element: <MyPlaylists />,
-    icon: BsCollectionPlay
+    element: MyPlaylists,
+    icon: BsCollectionPlay,
   },
   {
     name: 'Watch Later',
     path: 'saved',
-    element: <WatchLater />,
-    icon: HiOutlineSave
+    element: WatchLater,
+    icon: HiOutlineSave,
   },
   {
-    name: "Trailer of the Day",
+    name: 'Trailer of the Day',
     path: 'watch/totd',
-    element: <WatchVideo />,
-    icon: MdMovieFilter
+    element: WatchVideo,
+    icon: MdMovieFilter,
   },
-]
+];
 
 export const AppRoutes = [
   {
     name: 'Home',
     path: '/',
-    element: <Home />,
-    icon: BiHomeAlt
+    element: Home,
+    icon: BiHomeAlt,
   },
   {
     name: 'Trending',
     path: 'trending',
-    element: <Trending />,
-    icon: HiFire
+    element: Trending,
+    icon: HiFire,
   },
   {
     name: 'My Playlists',
     path: 'playlists',
-    element: <MyPlaylists />,
-    icon: BsCollectionPlay
+    element: MyPlaylists,
+    icon: BsCollectionPlay,
   },
   {
     name: 'Watch Later',
     path: 'saved',
-    element: <WatchLater />,
-    icon: HiOutlineSave
+    element: WatchLater,
+    icon: HiOutlineSave,
   },
-]
+];
