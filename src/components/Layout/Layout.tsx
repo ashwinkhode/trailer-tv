f6f03f7d56d6a91fd212fe499d4e9d41ea7b0328
Ashwin Khode <ashwin.khode1@gmail.com>
2021-07-18 00:16:39 +0530

    fix: support mobile view

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,17 +7,22 @@ export const Layout = ({ children }: any, ...otherprops: any[]) => {
   const [menuToggle, setMenuToggle] = useState(false);
   return (
     <div
-      className="w-full h-full flex justify-start items-center rounded-3xl bg-black bg-opacity-60 bg-blur overflow-hidden"
+      className="max-w-[1280px] w-full h-full  lg:flex-row lg:justify-between lg:rounded-3xl bg-black bg-opacity-60 bg-blur overflow-hidden"
       {...otherprops}
     >
-      <div className={clsx('h-full w-[5%]')}>
-        <Sidenav menuToggle={menuToggle} setMenuToggle={setMenuToggle} />
-      </div>
       <div
-        className="w-[95%] h-full flex flex-col justify-start items-start"
-        onClick={() => setMenuToggle(false)}
+        className={clsx(
+          'max-w-full w-full h-[10%] lg:h-full lg:w-[5%] relative z-10'
+        )}
       >
-        <Header />
+        <div className="fixed top-0 w-full">
+          <Header />
+        </div>
+        <div className="fixed bottom-0 w-full">
+          <Sidenav menuToggle={menuToggle} setMenuToggle={setMenuToggle} />
+        </div>
+      </div>
+      <div className="min-w-full w-screen h-screen overflow-y-auto">
         {children}
       </div>
     </div>
