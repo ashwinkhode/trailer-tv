547196c71b4bb005a8299ecb4a586eb9542117a6
Ashwin Khode <ashwin.khode1@gmail.com>
2021-07-29 15:19:14 +0530

    fix: layout for lg displays

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,22 +7,22 @@ export const Layout = ({ children }: any, ...otherprops: any[]) => {
   const [menuToggle, setMenuToggle] = useState(false);
   return (
     <div
-      className="max-w-[1280px] w-full h-full  lg:flex-row lg:justify-between lg:rounded-3xl bg-black bg-opacity-60 bg-blur overflow-hidden"
+      className="relative max-w-[1280px] w-full h-full  lg:flex-row lg:justify-between lg:mx-auto lg:rounded-3xl bg-black bg-opacity-60 bg-blur overflow-hidden"
       {...otherprops}
     >
       <div
         className={clsx(
-          'max-w-full w-full h-[10%] lg:h-full lg:w-[5%] relative z-10'
+          'max-w-full w-full h-[10%] lg:h-full lg:w-[5.5%] relative z-10'
         )}
       >
-        <div className="fixed top-0 w-full">
+        <div className="fixed top-0 w-full lg:pl-16">
           <Header />
         </div>
-        <div className="fixed bottom-0 w-full">
+        <div className="fixed bottom-0 w-full lg:relative lg:w-auto lg:h-full">
           <Sidenav menuToggle={menuToggle} setMenuToggle={setMenuToggle} />
         </div>
       </div>
-      <div className="min-w-full w-screen h-screen overflow-y-auto">
+      <div className="min-w-full w-full h-full overflow-y-auto lg:absolute lg:inset-8 lg:px-8">
         {children}
       </div>
     </div>
