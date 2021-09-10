c784067930c257f8d06c606936a08f2c2f312706
Ashwin Khode <ashwin.khode1@gmail.com>
2021-09-10 15:41:01 +0530

    fix: bg image animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { Layout } from './components/Layout/Layout';
 import { useHome } from './context/HomeContext';
@@ -6,17 +7,33 @@ import Playlist from './pages/Playlist';
 import UserProfile from './pages/UserProfile';
 import { AppRoutes } from './utils/router';
 
+const BG = [
+  'https://trailer-tv.vercel.app/6023a91cdf5c539e0c5b.jpg',
+  'https://trailer-tv.vercel.app/0dee68533c000759c0dd.jpg',
+  'https://trailer-tv.vercel.app/1291aca6f65e44a5451d.jpg',
+  'https://trailer-tv.vercel.app/41b4e2ddfb6fcd33937a.jpg',
+  'https://trailer-tv.vercel.app/c94397de3c1cecbad49a.jpg',
+];
+
 export default function App() {
   const router = useLocation();
   const background = useHome();
 
+  useEffect(() => {
+    BG.forEach((picture) => {
+      const img = new Image();
+      img.src = picture;
+    });
+  });
+
   return (
-    <div
-      className={clsx(
-        router.pathname === '/' ? `${background.bg}` : 'bg-default',
-        'w-full h-full bg-center bg-no-repeat bg-cover lg:p-12'
-      )}
-    >
+    <div className="relative w-screen h-screen lg:p-12 overflow-hidden App">
+      <div
+        className={clsx(
+          router.pathname === '/' ? `${background.bg}` : 'bg-default',
+          'bgTransition absolute inset-0 w-full h-full bg-top bg-no-repeat bg-cover'
+        )}
+      ></div>
       <Layout>
         <Routes>
           <Route path="profile" element={<UserProfile />} />
